f60de51d620119628fb95f76f83160eb2409e94e
Felipe Augusto <augusto_felipe@icloud.com>
2022-09-03 15:59:16 -0300

    Gerando número aleatório

--- GeradorDeNumeros/ViewController.swift
+++ GeradorDeNumeros/ViewController.swift
@@ -9,11 +9,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var legendaResultado: UILabel!
+    @IBOutlet weak var imagemBotao: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    
+    @IBAction func gerarNumero(_ sender: Any) {
+        
+        let numero = arc4random_uniform(11)
+        legendaResultado.text = String(numero)
+        
+    }
+    
 }
 
